97eb14add1655fab41ec8e5130f3996455e2d2f1
Evan Butler <evan.butler@icloud.com>
2019-04-29 10:16:01 +0700

    PriceEx - Implemented tests

--- CSharp/VirtoCommerce.PriceExModule.Template/Test/Tests.cs
+++ CSharp/VirtoCommerce.PriceExModule.Template/Test/Tests.cs
@@ -1,4 +1,7 @@
-﻿using Xunit;
+﻿using $ext_safeprojectname$.Core.Model;
+using $ext_safeprojectname$.Data.Model;
+using VirtoCommerce.Platform.Core.Common;
+using Xunit;
 
 namespace $safeprojectname$
 {
@@ -9,9 +12,26 @@ namespace $safeprojectname$
         }
 
         [Fact]
-        public void Run_Test()
+        public void Convert_PriceEx_DTOToEntity_EntityToDTO()
         {
-            Assert.Equal(0, 0);
+            const decimal outerPrice = 1.0M;
+
+            var model = new PriceEx()
+            {
+                BasePrice = outerPrice
+            };
+
+            var pkMap = new PrimaryKeyResolvingMap();
+            var entityForPatch = (PriceExEntity)AbstractTypeFactory<PriceExEntity>.TryCreateInstance().FromModel(model, pkMap);
+
+            var entity = AbstractTypeFactory<PriceExEntity>.TryCreateInstance();
+            entityForPatch.Patch(entity);
+
+            var modelResult = (PriceEx)entity.ToModel(AbstractTypeFactory<PriceEx>.TryCreateInstance());
+
+            Assert.Equal(outerPrice, entityForPatch.BasePrice);
+            Assert.Equal(outerPrice, entity.BasePrice);
+            Assert.Equal(outerPrice, modelResult.BasePrice);
         }
     }
 }
