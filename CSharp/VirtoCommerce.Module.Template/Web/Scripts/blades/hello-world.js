28b772d9b9dace6fcd5b8b8e32fa3c82f95a9058
Maks <maks_kopnov@rambler.ru>
2019-09-27 14:47:54 +0700

    Blade localization added

--- CSharp/VirtoCommerce.Module.Template/Web/Scripts/blades/hello-world.js
+++ CSharp/VirtoCommerce.Module.Template/Web/Scripts/blades/hello-world.js
@@ -1,10 +1,11 @@
-﻿angular.module('$ext_safeprojectnamecamel$')
+angular.module('$ext_safeprojectnamecamel$')
     .controller('$ext_safeprojectnamecamel$.helloWorldController', ['$scope', '$ext_safeprojectnamecamel$.webApi', function ($scope, api) {
         var blade = $scope.blade;
         blade.title = '$ext_projectname$';
 
         blade.refresh = function () {
             api.get(function (data) {
+                blade.title = '$ext_safeprojectnamecamel$.blades.hello-world.title'
                 blade.data = data.result;
                 blade.isLoading = false;
             });
